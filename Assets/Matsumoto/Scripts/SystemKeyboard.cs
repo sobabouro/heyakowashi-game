ff16dfcd35c0752e84e4243069378879bfb48f0c
Kouki Matsumoto <t231d069@gunma-u.ac.jp>
2024-08-15 20:10:49 +0900

    リザルト画面の試作及びシステムキーボードのテスト

---
+++ Assets/Matsumoto/Scripts/SystemKeyboard.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using MixedReality.Toolkit.Input;
+using MixedReality.Toolkit.UX;
+
+public class SystemKeyboard : MonoBehaviour
+{
+    [SerializeField]
+    private TouchScreenKeyboard keyboard;
+
+    [SerializeField]
+    private TextMeshPro debugMessage = null;
+
+    [SerializeField]
+    private KeyboardPreview mixedRealityKeyboardPreview = null;
+
+    private void Start()
+    {
+        // Initially hide the preview.
+        if (mixedRealityKeyboardPreview != null)
+        {
+            mixedRealityKeyboardPreview.gameObject.SetActive(false);
+        }
+    }
+
+    public void OpenSystemKeyboard()
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false);
+        Debug.Log("OpenSystemKeyboard");
+    }
+
+    private void Update()
+    {
+        if (keyboard != null)
+        {
+            string keyboardText = keyboard.text;
+            // Do stuff with keyboardText
+
+            if (TouchScreenKeyboard.visible)
+            {
+                if (debugMessage != null)
+                {
+                    debugMessage.text = "typing... " + keyboardText;
+                }
+            }
+            else
+            {
+                if (debugMessage != null)
+                {
+                    debugMessage.text = "typed " + keyboardText;
+                }
+
+                keyboard = null;
+            }
+        }
+    }
+}
