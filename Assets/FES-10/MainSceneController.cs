46140cebb9d047ee24c9d37110af950c9468ac7c
Adonis0k <shinta110121@gmail.com>
2024-09-09 18:16:24 +0900

    update: 制限時間はMainSceneControllerに実装する

--- Assets/FES-10/MainSceneController.cs
+++ Assets/FES-10/MainSceneController.cs
@@ -6,4 +6,47 @@ public class MainSceneController : MonoBehaviour
 {
     [SerializeField]
     private ScoreController scoreController;
+
+    [SerializeField]
+    private int _startTime = 0;
+
+    private float _nowTime;
+    private bool enableCountdown;
+
+    public float NowTime
+    {
+        get => _nowTime;
+        set => _nowTime = value;
+    }
+
+    private void Start()
+    {
+        _nowTime = _startTime;
+        ScoreController.Init();
+    }
+
+    private void Update()
+    {
+        if (!enableCountdown) return;
+        // カウントダウン
+        _nowTime -= Time.deltaTime;
+        if (_nowTime < 0) { _nowTime = 0; }
+
+    }
+
+    /// <summary>
+    /// ゲーム開始時処理
+    /// </summary>
+    private void StartGame()
+    {
+        enableCountdown = true;
+    }
+
+    /// <summary>
+    /// ゲーム終了時処理
+    /// </summary>
+    private void FinishGame()
+    {
+        enableCountdown = false;
+    }
 }
