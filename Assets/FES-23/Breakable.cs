f3a510f3a2ab8d17f3399a14f9bdf846606758bb
Adonis0k <shinta110121@gmail.com>
2024-09-25 17:09:50 +0900

    update: ダメージ計算する

--- Assets/FES-23/Breakable.cs
+++ Assets/FES-23/Breakable.cs
@@ -29,4 +29,18 @@ public class Breakable : MonoBehaviour
 		resists.Add(Type.crash, crashResist);
 		resists.Add(Type.pierce, pierceResist);
     }
+
+    /// <summary>
+    /// 与えられた攻撃力と属性、自身の耐性、最終的なダメージの値を計算する。
+    /// </summary>
+    /// <param name="receivedATK">受ける攻撃力</param>
+    /// <param name="attackType">受ける攻撃の属性</param>
+    /// <returns></returns>
+    private int CalcDamage(int receivedATK, Type attackType)
+	{
+		int damage = receivedATK - resists[attackType];
+		if (damage < 0) damage = 0;
+        return damage;
+	}
+
 }
