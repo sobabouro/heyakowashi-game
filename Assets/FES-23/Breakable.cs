e7f4cf72389c3f34d36fac7b6b19178440b66bab
Adonis0k <shinta110121@gmail.com>
2024-09-25 16:37:42 +0900

    refactor: 属性耐性を辞書型にしたり

--- Assets/FES-23/Breakable.cs
+++ Assets/FES-23/Breakable.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Type { plane, slash, crash, pierce }
+
 public class Breakable : MonoBehaviour
 {
 	[SerializeField, Tooltip("耐久値")]
 	private int durability = default;
-	[SerializeField, Tooltip("切断耐性"), Header("属性耐性")]
+	[Header("属性耐性")]
+	[SerializeField, Tooltip("切断耐性")]
 	private int slashResist = default;
 	[SerializeField, Tooltip("衝撃耐性"),]
 	private int crashResist = default;
@@ -15,4 +18,15 @@ public class Breakable : MonoBehaviour
 	[SerializeField, Tooltip("スコア")]
 	private int score = default;
 
+    // 属性耐性の辞書
+    private Dictionary<Type, int> resists = new Dictionary<Type, int>();
+    // 結合しているときの結合相手のBreakerクラス
+    // private Breaker connectedMainObj = null;
+
+    private void Start()
+    {
+		resists.Add(Type.slash, slashResist);
+		resists.Add(Type.crash, crashResist);
+		resists.Add(Type.pierce, pierceResist);
+    }
 }
