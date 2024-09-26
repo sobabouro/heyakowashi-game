20f8650cfc1c93e66ca7d5ff314cc5504ec7d25d
Adonis0k <shinta110121@gmail.com>
2024-09-26 14:19:58 +0900

    update: 最終攻撃力の計算

--- Assets/FES-22/Breaker.cs
+++ Assets/FES-22/Breaker.cs
@@ -21,4 +21,14 @@ public class Breaker : MonoBehaviour
     [SerializeField]
     private Rigidbody my_Rigitbody;
 
+
+
+    private int CalcATK(float other_velocity)
+    {
+        float velocity = my_Rigitbody.velocity.magnitude - other_velocity;
+        if (velocity < velocity_threshold) velocity = 0;
+        int finalATK = (int)(baseATK * my_Rigitbody.velocity.magnitude);
+        return finalATK;
+    }
+
 }
