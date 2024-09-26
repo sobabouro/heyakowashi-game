038a5808b25bda7df8a8ee0dca071241372fbd63
Adonis0k <shinta110121@gmail.com>
2024-09-26 16:16:49 +0900

    ix: plane属性の耐性を追加

--- Assets/FES-23/Breakable.cs
+++ Assets/FES-23/Breakable.cs
@@ -7,17 +7,17 @@ public enum Type { plane, slash, crash, pierce }
 
 public class Breakable : MonoBehaviour
 {
-	[SerializeField, Tooltip("耐久値")]
-	private int durability = default;
-	[Header("属性耐性")]
-	[SerializeField, Tooltip("切断耐性")]
-	private int slashResist = default;
-	[SerializeField, Tooltip("衝撃耐性"),]
-	private int crashResist = default;
-	[SerializeField, Tooltip("貫通耐性"),]
-	private int pierceResist = default;
-	[SerializeField, Tooltip("スコア")]
-	private int score = default;
+    [SerializeField, Tooltip("耐久値")]
+    private int durability = default;
+    [Header("属性耐性")]
+    [SerializeField, Tooltip("切断耐性")]
+    private int slashResist = default;
+    [SerializeField, Tooltip("衝撃耐性"),]
+    private int crashResist = default;
+    [SerializeField, Tooltip("貫通耐性"),]
+    private int pierceResist = default;
+    [SerializeField, Tooltip("スコア")]
+    private int score = default;
 
     // 属性耐性の辞書
     private Dictionary<Type, int> resists = new Dictionary<Type, int>();
@@ -26,9 +26,10 @@ public class Breakable : MonoBehaviour
 
     private void Start()
     {
-		resists.Add(Type.slash, slashResist);
-		resists.Add(Type.crash, crashResist);
-		resists.Add(Type.pierce, pierceResist);
+        resists.Add(Type.slash, slashResist);
+        resists.Add(Type.crash, crashResist);
+        resists.Add(Type.pierce, pierceResist);
+        resists.Add(Type.plane, 0);
     }
 
 
@@ -39,13 +40,14 @@ public class Breakable : MonoBehaviour
     /// <param name="receivedATK">受ける攻撃力</param>
     /// <param name="breaker">攻撃した側の情報</param>
     /// <returns></returns>
-    private bool ReciveAttack(int receivedATK, Breaker breaker)
+    public bool ReciveAttack(int receivedATK, Breaker breaker)
     {
         int damage = CalcDamage(receivedATK, breaker.Type);
         Debug.Log($"damage: {damage}");
         durability -= damage;
         Debug.Log($"durability: {durability}");
-        if (durability < 0) {
+        if (durability < 0)
+        {
             Break(breaker);
             return true;
         }
@@ -62,8 +64,8 @@ public class Breakable : MonoBehaviour
         // addScore(_socre) 
         // Breaker.enable = ture;
         switch (breaker.Type)
-		{
-			case Type.slash:
+        {
+            case Type.slash:
                 // Slashクラスを呼び出す
                 Destroy(this.gameObject);
                 break;
@@ -75,7 +77,7 @@ public class Breakable : MonoBehaviour
                 // Pierceクラスを呼び出す
                 break;
             default:
-				break;
+                break;
         }
     }
 
@@ -87,10 +89,10 @@ public class Breakable : MonoBehaviour
     /// <param name="attackType">受ける攻撃の属性</param>
     /// <returns></returns>
     private int CalcDamage(int receivedATK, Type attackType)
-	{
-		int damage = receivedATK - resists[attackType];
-		if (damage < 0) damage = 0;
+    {
+        int damage = receivedATK - resists[attackType];
+        if (damage < 0) damage = 0;
         return damage;
-	}
+    }
 
 }
