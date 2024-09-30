0808732daeded2810abde9a772055ee0301131b2
Kouki Matsumoto <t231d069@gunma-u.ac.jp>
2024-09-30 15:25:25 +0900

    save: 当たり判定とそのヒエラルキーの構造のテスト

---
+++ Assets/Matsumoto/Scripts/OnCollisionEnterTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionEnterTest : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject + "Ç…ìñÇΩÇ¡ÇƒÇ¢ÇÈÅI");
+    }
+}
