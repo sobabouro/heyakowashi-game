8b1ff7296f04d877c57f4b6a53ef3e19e5851aae
Kouki Matsumoto <t231d069@gunma-u.ac.jp>
2024-09-30 18:19:50 +0900

    add: Pierceクラスの作成

---
+++ Assets/FES-7/Pierce.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pierce : MonoBehaviour
+{
+    [SerializeField]
+    private int durabilityRecoveryAmount;
+
+    public void Connect(Breaker breaker)
+    {
+
+    }
+
+    private Vector3 DecideConnectPosition()
+    {
+        Vector3 connectPosition = new Vector3();
+
+        return connectPosition;
+    }
+}
