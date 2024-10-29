79a977359867eb215dc10b751ba3360ed559fd43
Kouki Matsumoto <t231d069@gunma-u.ac.jp>
2024-10-29 15:05:13 +0900

    fix: 切断クラスの統合、ゲームスタート前にイベントが発生する事象の修正

--- Assets/FES-21/CollisionEvent.cs
+++ Assets/FES-21/CollisionEvent.cs
@@ -8,22 +8,27 @@ public class CollisionEvent : MonoBehaviour
 {
     [Serializable] public class UnityEventArgCollision : UnityEvent<Collision> { }
 
+    public static bool canEventCall;
+
     [SerializeField] public UnityEventArgCollision collisionEvnetEnter;
     [SerializeField] public UnityEventArgCollision collisionEvnetStay;
     [SerializeField] public UnityEventArgCollision collisionEvnetExit;
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (!canEventCall) return;
         collisionEvnetEnter.Invoke(collision);
     }
 
     private void OnCollisionStay(Collision collision)
     {
+        if (!canEventCall) return;
         collisionEvnetEnter.Invoke(collision);
     }
 
     private void OnCollisionExit(Collision collision)
     {
+        if (!canEventCall) return;
         collisionEvnetEnter.Invoke(collision);
     }
 }
